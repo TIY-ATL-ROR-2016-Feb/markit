cfe5142d46e9856268bcd00cd89515b653226b7d
Brit Butler <redline6561@gmail.com>
2016-04-11 11:57:39 -0400

    Add email and password validation tests.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,19 @@ class UserTest < ActiveSupport::TestCase
     assert_not_nil @user.password_digest
     assert_not_equal password, @user.password_digest
   end
+
+  test "users can't be saved without a password" do
+    @user = User.new(email: "cookies@monster.whatever")
+    refute @user.valid?
+    @user.password = "lunchtime"
+    assert @user.valid?
+  end
+
+  test "users with an invalid email are rejected" do
+    @user = User.new(email: "stupid nonsense",
+                     password: "taco lunch")
+    refute @user.valid?
+    @user.email = "this@is.okay"
+    assert @user.valid?
+  end
 end
