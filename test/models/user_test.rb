16d503868e90be30b33a55a9028ecde851a6ca5d
Brit Butler <redline6561@gmail.com>
2016-04-11 10:05:26 -0400

    Add password encryption test.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user passwords are encrypted" do
+    password = "cookies"
+    @user = User.new(email: "foo@bar.baz",
+                     password: password)
+    @user.save
+    assert_nil @user.password
+    assert_not_nil @user.password_digest
+    assert_not_equal password, @user.password_digest
+  end
 end
