0398e2440f805de6572557fc0f9e4848583af917
Brit Butler <redline6561@gmail.com>
2016-04-12 11:04:16 -0400

    Add a test for the belongs_to association and fix up the model.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+
+  # test "can create a basic post" do
+  #   @post = Post.new(user_id: users(:brit).id,
+  #                    title: "awesome cats",
+  #                    link_url: "http://http://i.imgur.com/2Hnwn3eg.jpg")
+  #   assert @post.save
   # end
+
+  test "posts must have a user_id" do
+    @post = Post.new(title: "awesome cats",
+                     link_url: "google.com")
+    refute @post.valid?, "saved without a user_id"
+  end
+
+  test "posts have a user" do
+    post = posts(:google)
+    assert_equal post.user, users(:brit)
+  end
 end
